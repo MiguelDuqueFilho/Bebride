478f613a00a5c6a91bad8b304ce5a6992fab3118
Miguel Duque Filho <miguel.duque@globo.com>
2019-08-31 00:04:26 -0300

    carousel depoimentos ajustes

--- public_html/assets/site/js/bebride-carousel.js
+++ public_html/assets/site/js/bebride-carousel.js
@@ -1,7 +1,7 @@
 /*
     Carousel
 */
-$('#carousel-example').on('slide.bs.carousel', function (e) {
+$('#carousel-depositions').on('slide.bs.carousel', function (e) {
   /*
       CC 2.0 License Iatek LLC 2018 - Attribution required
   */
@@ -10,12 +10,27 @@ $('#carousel-example').on('slide.bs.carousel', function (e) {
   var itemsPerSlide = 4;
   var totalItems = $('.carousel-item').length;
 
+  var element = document.getElementById("carousel-depositions");
+
+  if (window.innerWidth <= 576)
+  {
+    element.classList.remove("slide");
+    console.log('class remove style: ' + element);
+  }
+  else
+  {
+    element.classList.add("slide");
+    console.log('class add style: ' + element);
+  }
+
+
   if (idx >= totalItems-(itemsPerSlide-1)) {
       var it = itemsPerSlide - (totalItems - idx);
       for (var i=0; i<it; i++) {
           // append slides to end
           if (e.direction=="left") {
               $('.carousel-item').eq(i).appendTo('.carousel-inner');
+  
           }
           else {
               $('.carousel-item').eq(0).appendTo('.carousel-inner');
@@ -23,3 +38,6 @@ $('#carousel-example').on('slide.bs.carousel', function (e) {
       }
   }
 });
+
+
+  
